31660f71eab5b54d69c8441c3dda677fa8722842
Paulo Roberto <paulo.trabalho10@gmail.com>
2023-04-26 12:05:15 -0300

    projeto IMC
    primeiro commit criando o ambiente de trabalho.

---
+++ projetos/my-project/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Title from './src/components/Title/';
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Title/>  
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
